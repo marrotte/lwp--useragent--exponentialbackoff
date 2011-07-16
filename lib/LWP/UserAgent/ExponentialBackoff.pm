741dd651a6a92badc55e379507beeb1e82a79c1d
Michael Marrotte <marrotte@gmail.com>
2011-07-16 10:52:43 -0400

    Added callbacks before_request and after request

--- lib/LWP/UserAgent/ExponentialBackoff.pm
+++ lib/LWP/UserAgent/ExponentialBackoff.pm
@@ -2,12 +2,12 @@ use LWP::UserAgent;
 
 package LWP::UserAgent::ExponentialBackoff;
 $VERSION = '1.00';
-@ISA = ("LWP::UserAgent");
-my @FAILCODES = qw(302 500 503);
+@ISA     = ("LWP::UserAgent");
+my @FAILCODES = qw(408 500 502 503 504);
 
 sub new {
 	my ( $class, %cnf ) = @_;
-	my $sum = delete $cnf{sum};
+	my $sum        = delete $cnf{sum};
 	my $retryCount = delete $cnf{retryCount};
 	$retryCount = 3 unless defined $retryCount;
 	my $minBackoff = delete $cnf{minBackoff};
@@ -19,7 +19,7 @@ sub new {
 	my $tolerance = delete $cnf{tolerance};
 	$tolerance = .20 unless defined $tolerance;
 	my $failCodes = delete $cnf{failCodes};
-	$failCodes =  {map { $_ => $_ } @FAILCODES} unless defined $failCodes;
+	$failCodes = { map { $_ => $_ } @FAILCODES } unless defined $failCodes;
 	my $self = $class->SUPER::new(@_);
 	$self = bless {
 		%{$self},
@@ -31,8 +31,7 @@ sub new {
 		deltaBackoff => $deltaBackoff,
 		deltaLow     => $deltaLow,
 		deltaHigh    => $deltaHigh,
-		failCodes    => $failCodes,
-		retries      => []
+		failCodes    => $failCodes
 	}, $class;
 	$self->deltas($tolerance);
 	$self->sum($sum) unless !defined $sum;
@@ -40,32 +39,34 @@ sub new {
 }
 
 sub simple_request {
-	my ( $self, $class ) = @_;
-	my $total = 0;
+	my ( $self, @args ) = @_;
+	my $total             = 0;
 	my $failCodes         = $self->{failCodes};
 	my $currentRetryCount = 0;
-	my $class             = shift;
-	$response = $class->SUPER::simple_request(@_);
-	my $code              = $response->code();
-	while ( ( $retryInterval = $self->retry($currentRetryCount) )
-		&& ${$failCodes}{$code} )
-	{
-		$self->{retries}[$currentRetryCount]=$retryInterval;
-		$currentRetryCount++;
+	my $before_c          = $self->before_request;
+	my $after_c           = $self->after_request;
+	my $retryInterval     = 0;
+	
+	do {
 		sleep $retryInterval;
-		$response = $self->SUPER::simple_request(@_);
-		$code     = $response->code();
-	}
+		$before_c->( $self, \@args );
+		$response = $self->SUPER::simple_request(@args);
+		$after_c and $after_c->( $self, \@args, $response );
+		$code = $response->code();
+		$currentRetryCount++;
+	}while ( ( $retryInterval = $self->retry($currentRetryCount-1) )
+		&& ${$failCodes}{$code} );
+		
 	return $response;
 }
 
 sub retry {
 	my ( $self, $currentRetryCount ) = @_;
-	my $retryCount = $self->{retryCount};
-	my $minBackoff = $self->{minBackoff};
-	my $maxBackoff = $self->{maxBackoff};
-	my $deltaLow   = $self->{deltaLow};
-	my $deltaHigh  = $self->{deltaHigh};
+	my $retryCount   = $self->{retryCount};
+	my $minBackoff   = $self->{minBackoff};
+	my $maxBackoff   = $self->{maxBackoff};
+	my $deltaLow     = $self->{deltaLow};
+	my $deltaHigh    = $self->{deltaHigh};
 	my $deltaBackoff = $self->{deltaBackoff};
 
 	if ( $currentRetryCount < $retryCount ) {
@@ -84,9 +85,12 @@ sub retry {
 	return $retryInterval;
 }
 
-sub retryCount { shift->_elem( 'retryCount', @_ ); }
-sub minBackoff { shift->_elem( 'minBackoff', @_ ); }
-sub maxBackoff { shift->_elem( 'maxBackoff', @_ ); }
+sub retryCount            { shift->_elem( 'retryCount',            @_ ); }
+sub minBackoff            { shift->_elem( 'minBackoff',            @_ ); }
+sub maxBackoff            { shift->_elem( 'maxBackoff',            @_ ); }
+sub failCodes             { shift->_elem( 'failCodes',             @_ ) }
+sub before_request { shift->_elem( 'before_request', @_ ) }
+sub after_request  { shift->_elem( 'after_request',  @_ ) }
 
 sub tolerance {
 	my ( $self, $tolerance ) = @_;
@@ -118,17 +122,18 @@ sub delFailCodes {
 	delete $self->{failCodes}->{$code};
 }
 
-# Given sum and deltaBackoff, compute retryCount and maxBackoff such that total retryIntervals will always be equal to or "slightly" greater than sum.  
+# Given sum and deltaBackoff, compute retryCount and maxBackoff such that total retryIntervals will always be equal to or "slightly" greater than sum.
 sub sum {
 	my ( $self, $sum ) = @_;
 	$self->{retryCount} = log2( $sum / $self->{deltaBackoff} - 1 );
-	# maxBackoff should be at least as big as the largest retry interval which is never bigger than the sum, so just make it equal the sum
+
+# maxBackoff should be at least as big as the largest retry interval which is never bigger than the sum, so just make it equal the sum
 	$self->{maxBackoff} = $sum;
+	$self->{sum} = $sum;
 }
 
 sub log2 {
 	my $n = shift;
 	return log($n) / log(2);
 }
-
 
